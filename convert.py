bab95f5596535adb3d3ab670a1944a27a4308b9c
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-11-21 20:53:59 +0530

    Add code to generate annotation video
    Combining the annotation with hvec videos doesn't seem to work correctly.
    This is still not usable.

--- convert.py
+++ convert.py
@@ -9,10 +9,19 @@ import re
 import subprocess
 from urllib.parse import unquote
 
-INPUT_TXT = "input.txt"
-OUTPUT_FILE = "output.mkv"
+from PIL import Image, ImageDraw, ImageFont
+import yaml
 
-IGNORE_EXTENSIONS = (".txt", ".jpg")
+
+INPUT_TXT = "input.txt"
+OUTPUT_NAME = "output.mkv"
+CONFIG_NAME = "config.yaml"
+COVER_NAME = "cover.png"
+HERE = os.path.dirname(os.path.abspath(__file__))
+LOGO_FILE = os.path.join(HERE, "logo.jpg")
+AUDIO_FILE = os.path.join(HERE, "audio.mp3")
+IGNORE_EXTENSIONS = (".txt", ".jpg", ".png", ".yaml")
+CODEC_MAP = {"h264": "libx264", "hevc": "libx265"}
 
 
 def _video_number(filename):
@@ -30,6 +39,91 @@ def _get_video_list(video_dir):
     return videos
 
 
+def generate_cover_image(video_dir, width, height):
+    img = Image.new("RGB", (width, height), color=(255, 255, 255))
+
+    with open(os.path.join(video_dir, CONFIG_NAME)) as f:
+        config = yaml.load(f.read(), Loader=yaml.SafeLoader)
+
+    if not {"title", "date", "venue"}.issubset(config.keys()):
+        raise RuntimeError("Need title, date and venue in config")
+
+    # Insert title, date and venue
+    title_fnt = ImageFont.truetype("Ubuntu-R.ttf", 40)
+    text_fnt = ImageFont.truetype("Ubuntu-R.ttf", 20)
+
+    title, date, venue = config["title"], config["date"], config["venue"]
+
+    t_w, t_h = title_fnt.getsize(title)
+    d_w, d_h = text_fnt.getsize(date)
+    v_w, v_h = text_fnt.getsize(venue)
+
+    title_x = (width - t_w) / 2
+    title_y = height / 2
+
+    date_x = (width - d_w) / 2
+    date_y = title_y + t_h + 10
+
+    venue_y = date_y + d_h + 10
+    venue_x = (width - v_w) / 2
+
+    d = ImageDraw.Draw(img)
+    d.text((title_x, title_y), title, font=title_fnt, fill=(110, 110, 110))
+    d.text((date_x, date_y), date, font=text_fnt, fill=(110, 110, 110))
+    d.text((venue_x, venue_y), venue, font=text_fnt, fill=(110, 110, 110))
+
+    # Insert logo
+    logo = Image.open(LOGO_FILE, "r")
+    logo_w, logo_h = logo.size
+    offset = ((width - logo_w - 10), (height - logo_h - 10))
+    img.paste(logo, offset)
+
+    path = os.path.join(video_dir, COVER_NAME)
+    img.save(path)
+    return path
+
+
+def generate_annotation(video_dir):
+    videos = _get_video_list(video_dir)
+    command_fmt = (
+        "ffprobe -v error -select_streams {stream}:0 -show_entries stream={params} "
+        "-of default=noprint_wrappers=1:nokey=1 {video}"
+    )
+    params = "codec_name,width,height"
+    command = command_fmt.format(stream="v", video=videos[0], params=params)
+    video_encoding, width, height = (
+        subprocess.check_output(command.split(), cwd=video_dir)
+        .strip()
+        .decode("utf8")
+        .split()
+    )
+    generate_cover_image(video_dir, int(width), int(height))
+
+    params = "codec_name"
+    command = command_fmt.format(stream="a", video=videos[0], params=params)
+    audio_encoding = (
+        subprocess.check_output(command.split(), cwd=video_dir)
+        .strip()
+        .decode("utf8")
+    )
+
+    cmd_fmt = (
+        "ffmpeg -framerate 0.2 -pattern_type glob -i {cover} "
+        "-i {audio} -c:a {a_codec} -shortest "
+        "-c:v {v_codec} -r 30 -pix_fmt yuv420p {fname}.{ext}"
+    )
+    video_ext = videos[0].rsplit(".", 1)[-1]
+    command = cmd_fmt.format(
+        cover=COVER_NAME,
+        audio=AUDIO_FILE,
+        a_codec=audio_encoding,
+        v_codec=CODEC_MAP[video_encoding],
+        fname="annotation_0",
+        ext=video_ext,
+    )
+    subprocess.check_output(command.split(), cwd=video_dir)
+
+
 def generate_concatenated_video(video_dir):
     """Generate a concatenated video from all videos in a directory.
 
@@ -37,17 +131,15 @@ def generate_concatenated_video(video_dir):
      the videos are not in the same format, this function cannot be used.
 
     """
-    if os.path.exists(os.path.join(video_dir, OUTPUT_FILE)):
-        print('Output video file "{}" already exists!'.format(OUTPUT_FILE))
+    output_file = os.path.abspath(os.path.join(video_dir, OUTPUT_NAME))
+    if os.path.exists(output_file):
+        print('"{}" already exists!'.format(output_file))
         return
     videos = _get_video_list(video_dir)
     if len(videos) == 1:
         video = videos[0]
-        print('Renaming single video "{}" to "{}"'.format(video, OUTPUT_FILE))
-        os.rename(
-            os.path.join(video_dir, video),
-            os.path.join(video_dir, OUTPUT_FILE),
-        )
+        print('Renaming single video "{}" to "{}"'.format(video, OUTPUT_NAME))
+        os.rename(os.path.join(video_dir, video), output_file)
         return
     formats = {v.rsplit(".")[-1] for v in videos}
     assert len(formats) == 1, "All videos must be in the same format!"
@@ -61,17 +153,20 @@ def generate_concatenated_video(video_dir):
 
     command_fmt = "ffmpeg -f concat -safe 0 -i {filelist} -c copy {output}"
     command = command_fmt.format(
-        filelist=INPUT_TXT, output=OUTPUT_FILE
+        filelist=INPUT_TXT, output=output_file
     ).split()
     subprocess.check_call(command, cwd=video_dir)
 
 
 def main(video_dir):
+    generate_annotation(video_dir)
     generate_concatenated_video(video_dir)
 
 
 if __name__ == "__main__":
     import sys
 
+    assert os.path.exists(AUDIO_FILE)
+    assert os.path.exists(LOGO_FILE)
     video_dir = sys.argv[1]
     main(video_dir)
